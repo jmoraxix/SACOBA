fa0da2838631ba189e290c0985bdd2ac4df2668d
Jose Mora <jdevmora@gmail.com>
2016-06-18 13:49:25 -0600

    - Creación del proyecto.

---
+++ SACOBA/src/sacoba/SACOBA.java
@@ -0,0 +1,20 @@
+/*
+ * SACOBA
+ * Jun 18, 2016
+ */
+package sacoba;
+
+/**
+ *
+ * @author jmora
+ */
+public class SACOBA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
