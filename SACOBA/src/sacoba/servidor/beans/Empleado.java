43a6f772469618025fddd7434f72937946a15065
mcascanteq054 <mcascante1410@gmail.com>
2016-08-06 17:27:47 -0600

    botones y empleado

--- SACOBA/src/sacoba/servidor/beans/Empleado.java
+++ SACOBA/src/sacoba/servidor/beans/Empleado.java
@@ -11,6 +11,47 @@ package sacoba.servidor.beans;
  */
 public class Empleado {
 
+    private String nombre, apellido1, apellido2, departamento, puesto;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
     
     
 }
