e6d93d661dcfc506aea4dc2639a44ecf7514bbe0
Jose Mora <jdevmora@gmail.com>
2016-08-12 00:34:51 -0600

    - Cambios en la estructura del Arbol
    *REVISAR*

---
+++ SACOBA/src/sacoba/servidor/estructuras/ListaEnlazadaHojas.java
@@ -0,0 +1,114 @@
+/*
+ *  Sistema de Asistencia de Control Bancario
+ *  SACOBA
+ *  
+ *  Marcela Cascante Quiros
+ *  Diego Delgado Cerdas
+ *  Alejandro Loaiza Arguedas
+ *  Jose David Mora Loria
+ */
+package sacoba.servidor.estructuras;
+
+/**
+ *
+ * @author jmora
+ */
+public class ListaEnlazadaHojas {
+
+    //Variables
+    NodoArbol cabeza;
+
+    //Constructor
+    public ListaEnlazadaHojas() {
+    }
+
+    public ListaEnlazadaHojas(NodoArbol cabeza) {
+        this.cabeza = cabeza;
+    }
+
+    //Metodos
+    public void insertaNodo(NodoArbol nodo) {
+        if (cabeza == null) {
+            cabeza = nodo;
+        } else {
+            NodoArbol aux = cabeza;
+
+            while (aux != null) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nodo);
+        }
+    }
+
+    public void modificaNombre(String nombreAnt, String nombreNuevo) {
+        if (cabeza != null) {
+            if (cabeza.getTitulo().equalsIgnoreCase(nombreAnt)) {
+                cabeza.setTitulo(nombreNuevo);
+            } else {
+                NodoArbol actual = cabeza.getSiguiente();
+                while (actual.getSiguiente() != null) {
+                    if (actual.getTitulo().equalsIgnoreCase(nombreAnt)) {
+                        actual.setTitulo(nombreNuevo);
+                    } else {
+                        actual = actual.getSiguiente();
+                    }
+                }
+            }
+        }
+    }
+
+    public void elimina(String nombre) {
+        if (cabeza != null) {
+            if (cabeza.getTitulo().equalsIgnoreCase(nombre)) {
+                cabeza = cabeza.getSiguiente();
+            } else {
+                NodoArbol anterior = cabeza;
+                NodoArbol actual = cabeza.getSiguiente();
+                while (actual.getSiguiente() != null) {
+                    if (actual.getTitulo().equalsIgnoreCase(nombre)) {
+                        anterior.setSiguiente(actual.getSiguiente());
+                        actual.setSiguiente(null);
+                    } else {
+                        anterior = actual;
+                        actual = actual.getSiguiente();
+                    }
+                }
+            }
+        }
+    }
+
+    public NodoArbol getHoja(String ID) {
+        NodoArbol buscado = null;
+        if (cabeza != null) {
+            if (cabeza.getId().equalsIgnoreCase(ID)) {
+                buscado = cabeza;
+            } else {
+                NodoArbol aux = cabeza;
+                while (aux.getSiguiente() != null) {
+                    if (aux.getId().equalsIgnoreCase(ID)) {
+                        buscado = aux;
+                    } else {
+                        aux = aux.getSiguiente();
+                    }
+                }
+            }
+        }
+        return buscado;
+    }
+
+    public boolean estaVacia() {
+        return cabeza == null;
+    }
+
+    @Override
+    public String toString() {
+        NodoArbol aux = cabeza;
+        String s = "Lista: \n";
+        while (aux != null) {
+            s += " -> " + aux + "\n";
+            aux = aux.getSiguiente();
+        }
+        return s;
+    }
+
+}
--- SACOBA/src/sacoba/servidor/estructuras/NodoArbol.java
+++ SACOBA/src/sacoba/servidor/estructuras/NodoArbol.java
@@ -9,45 +9,80 @@
  */
 package sacoba.servidor.estructuras;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author Marce
  */
 public class NodoArbol {
 
-    private Proceso raiz;
-    public ArrayList<NodoArbol> HijosList = new ArrayList<NodoArbol>();
-    private String info = "";
+    //Variables
+    private NodoArbol siguiente;
+    private ListaEnlazadaHojas procesos;
+    private ColaUsuarios colaUsuarios;
+    private final String id;
+    private String titulo;
+    private int totalClientes;
+    private int indice;
 
-    public NodoArbol(String theInfo) {
-        this.setInfo(theInfo);
+    //Constructor
+    public NodoArbol(String id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.indice = 0;
+        this.procesos = new ListaEnlazadaHojas();
     }
 
-    public void setInfo(String theInfo) {
-        this.info = theInfo;
+    //Getters & setters
+    public int getTotalClientes() {
+        return totalClientes;
     }
 
-    public String getInfo() {
-        return info;
-    }
-    
-
-    public Proceso getRaiz() {
-        return raiz;
+    public String getId() {
+        return id;
     }
 
-    public void setRaiz(Proceso raiz) {
-        this.raiz = raiz;
+    public String getTitulo() {
+        return titulo;
     }
 
-    public ArrayList<NodoArbol> getHijosList() {
-        return HijosList;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
-    public void setHijosList(ArrayList<NodoArbol> HijosList) {
-        this.HijosList = HijosList;
+    public NodoArbol getSiguiente() {
+        return siguiente;
     }
 
+    public void setSiguiente(NodoArbol siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    //Metodos
+    public NodoArbol getHoja(String id) {
+        return procesos.getHoja(id);
+    }
+
+    public boolean esHoja() {
+        return procesos.estaVacia();
+    }
+
+    public void insertarHoja(NodoArbol nodo) {
+        if (this.colaUsuarios.estaVacia()) { //Si la cola de usuarios no esta vacia, no se puede agregar otro hijo
+            if (procesos == null) {
+                this.procesos = new ListaEnlazadaHojas(nodo);
+            } else {
+                procesos.insertaNodo(nodo);
+            }
+        }
+    }
+
+//    public void insertarUsuario(Persona usuario) {
+//        if (this.esHoja()) {
+//            if (this.colaUsuarios == null) {
+//                this.colaUsuarios = new ColaUsuarios(new NodoCola(id.getBytes()[1] + "", usuario));
+//            } else {
+//                procesos.insertaNodo(nodo);
+//            }
+//        }
+//    }
 }
